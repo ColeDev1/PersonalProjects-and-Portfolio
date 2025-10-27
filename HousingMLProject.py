3b1455a31672bcfaacf2ebd505448f6aa469ac67
Cole <dontdont1234@gmail.com>
2025-10-27 18:01:21 -0400

    made significant progress by using scikit-learn transformers

--- HousingMLProject.py
+++ HousingMLProject.py
@@ -1,39 +1,59 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-#import matplotlib.pyplot as plt
-import random
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+import csv
 
-
+#open files
 kaggleData = pd.read_csv("train.csv")
 kaggleTestData = pd.read_csv("test.csv")
 
-
-kaggleData = kaggleData.fillna(0)
-
 Y_data = kaggleData[["SalePrice"]]
-X_data = kaggleData.drop(columns="SalePrice")
+kaggleData = kaggleData.drop(columns=["SalePrice","Id"])
 
-X_data = pd.get_dummies(X_data, drop_first=True)
+str_data_cols = (kaggleData.select_dtypes(include=['object', 'string']).columns)
+num_data_cols = (kaggleData.select_dtypes(exclude=['object', 'string']).columns)
 
-kaggleTestData = kaggleTestData.fillna(0)
-print(kaggleTestData.head())
-kaggleTestData  = pd.get_dummies(kaggleTestData, drop_first=True)
-print(kaggleTestData.head())
-modelA = LinearRegression()
-modelA.fit(X_data,Y_data)
+#(name,encoder,columns)
+processor = ColumnTransformer([("StringConverter",OneHotEncoder(drop=None, sparse_output=False),str_data_cols),
+                               ("NumConverter", StandardScaler(), num_data_cols)])
 
-differences = []
-for i in range(30):
-    index = random.randint(1,1000)
-    TestData = kaggleTestData.iloc[index]
-
-    output = modelA.predict(TestData)
-    print(output)
+fixedTrainingData = processor.fit_transform(kaggleData)  
+fixedTestingData = processor.fit_transform(kaggleTestData)
 
 
+# #clean training data
+# kaggleData = kaggleData.fillna(0)
 
-#print(kaggleData.head())
-# print(kaggleData.isnull().sum())
-print("No Errors!")
+# #retrieve prediction data type
+# #remove data for training
+# X_data = kaggleData.drop(columns=["SalePrice","Id"])
+# #set text data to numerical data for model,
+# #ex Street could be paved or gravel, the model wants numbers not text.
+# X_data = pd.get_dummies(X_data)
+
+
+# kaggleTestData = kaggleTestData.fillna(0)
+# kaggleTestData  = pd.get_dummies(kaggleTestData)
+# testDataNoID = kaggleTestData.drop(columns="Id")
+# print(str(set(X_data.columns)-set(kaggleTestData.columns)))
+# for col in set(set(X_data.columns)-set(kaggleTestData.columns)):
+#     kaggleTestData[str(col)] = False
+
+
+
+# modelA = LinearRegression() #simplest model to start
+# modelA.fit(X_data,Y_data)
+
+# with open('LR_submission.csv', mode="w") as file:
+#     file.write("Id, SalePrice\n")
+#     for i in range(len(kaggleTestData)):
+#         predicted_Id = kaggleTestData.iloc[i+1].loc["Id"]
+#         inputData = testDataNoID.iloc[[i+1]]
+#         outputData = modelA.predict(inputData)
+#         file.write(str(outputData))
+#         file.write(",")
+#         file.write(str(predicted_Id))
+#         file.write('\n')
+
+# # print("No Errors!")
